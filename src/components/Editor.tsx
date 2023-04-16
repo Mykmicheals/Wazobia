a9249ff58db29026a1d4154f0127124246038bca
Micheal <mykmicheals@gmail.com>
2023-04-16 18:17:51 +0100

    split editor dropdown

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,4 @@
 import { useState, useRef } from "react";
-import { AiFillPicture, AiFillVideoCamera } from "react-icons/ai";
-import { TiSocialFlickr } from "react-icons/ti";
 
 import VideoModal from "../Modals/VideoModal";
 import LinkModal from "../Modals/LinkModal";
@@ -17,6 +15,7 @@ import {
   modal1,
 } from "../utils/styles";
 import EditorConfig from "./EditorConfig";
+import EditorDropdown from "./EditorDropdown";
 
 const Editor = () => {
   const [showDropdown, setShowDropDown] = useState(false);
@@ -60,48 +59,11 @@ const Editor = () => {
       </div>
 
       {showDropdown ? (
-        <>
-          <div id="embeds" className={dropDown}>
-            <h4>Embeds</h4>
-            <div
-              onClick={() => {
-                setShowModal({ ...showModal, picture: true });
-                setShowDropDown(false);
-              }}
-              className={modal1}
-            >
-              <AiFillPicture className="my-1" />
-              <div>
-                <p>Picture</p>
-                <small>.jpeg, .png</small>
-              </div>
-            </div>
-            <div
-              onClick={() => {
-                setShowModal({ ...showModal, video: true });
-                setShowDropDown(false);
-              }}
-              className={modal1}
-            >
-              <AiFillVideoCamera className="my-1" />
-              <div>
-                <p>Video</p>
-                <small>Embed a youtube video</small>
-              </div>
-            </div>
-
-            <div
-              onClick={() => setShowModal({ ...showModal, link: true })}
-              className={modal1}
-            >
-              <TiSocialFlickr className="my-1" />
-              <div>
-                <p>Social</p>
-                <small>Embed a facebook link</small>
-              </div>
-            </div>
-          </div>
-        </>
+        <EditorDropdown
+          setShowModal={setShowModal}
+          setShowDropDown={setShowDropDown}
+          showModal={showModal}
+        />
       ) : null}
 
       <div>
